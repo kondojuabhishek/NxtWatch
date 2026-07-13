f6dfd5ad666fb5b8b07551cb69ab6fc1121a8598
Abhishek <kondojuabhishek.com>
2026-07-13 12:11:06 +0000

    Section 6: Added Trending Route

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,9 +1,150 @@
-// src/components/Home/index.js
 import {Component} from 'react'
+import {HiFire} from 'react-icons/hi'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import NxtWatchContext from '../../context/NxtWatchContext'
+import Header from '../Header'
+import Sidebar from '../Sidebar'
+import VideoCard from '../VideoCard'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
 
 class Trending extends Component {
+  state = {
+    videosList: [],
+    apiStatus: apiStatusConstants.initial,
+  }
+
+  componentDidMount() {
+    this.getVideos()
+  }
+
+  getVideos = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+
+    const jwtToken = Cookies.get('jwt_token')
+    const url = `https://apis.ccbp.in/videos/trending`
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+
+    const response = await fetch(url, options)
+
+    if (response.ok === true) {
+      const data = await response.json()
+
+      const formattedData = data.videos.map(each => ({
+        id: each.id,
+        title: each.title,
+        thumbnailUrl: each.thumbnail_url,
+        channel: {
+          name: each.channel.name,
+          profileImageUrl: each.channel.profile_image_url,
+        },
+        viewCount: each.view_count,
+        publishedAt: each.published_at,
+      }))
+
+      this.setState({
+        videosList: formattedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  onClickRetry = () => {
+    this.getVideos()
+  }
+
+  renderLoadingView = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
+
+  renderFailureView = () => (
+    <NxtWatchContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const failureImg = isDarkTheme
+          ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-dark-theme-img.png'
+          : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-light-theme-img.png'
+
+        return (
+          <div>
+            <img src={failureImg} alt="failure view" />
+            <h1>Oops! Something Went Wrong</h1>
+            <p>
+              We are having some trouble to complete your request. Please try
+              again.
+            </p>
+            <button type="button" onClick={this.onClickRetry}>
+              Retry
+            </button>
+          </div>
+        )
+      }}
+    </NxtWatchContext.Consumer>
+  )
+
+  renderSuccessView = () => {
+    const {videosList} = this.state
+
+    return (
+      <>
+        <div data-testid="banner">
+          <div>
+            <HiFire />
+          </div>
+          <h1>Trending</h1>
+        </div>
+        <ul>
+          {videosList.map(video => (
+            <VideoCard key={video.id} videoDetails={video} />
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  renderTrendingVideos = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      case apiStatusConstants.success:
+        return this.renderSuccessView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      default:
+        return null
+    }
+  }
+
   render() {
-    return <div>Trending</div>
+    return (
+      <>
+        <Header />
+        <div data-testid="trending">
+          <Sidebar />
+          <div className="trending-content">{this.renderTrendingVideos()}</div>
+        </div>
+      </>
+    )
   }
 }
 
