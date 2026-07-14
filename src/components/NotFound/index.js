a388c4fecb982463bf2919d1259ee58522f62eef
Abhishek <kondojuabhishek.com>
2026-07-14 15:32:02 +0000

    Section 10: Added NotFound Route

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,25 @@
-// src/components/Home/index.js
-import {Component} from 'react'
+import NxtWatchContext from '../../context/NxtWatchContext'
 
-class NotFound extends Component {
-  render() {
-    return <div>NotFound</div>
-  }
-}
+const NotFound = () => (
+  <NxtWatchContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      const notFoundImg = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-dark-theme-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-light-theme-img.png'
+
+      return (
+        <div>
+          <div>
+            <img src={notFoundImg} alt="not found" />
+            <h1>Page Not Found</h1>
+            <p>We are sorry, the page you requested could not be found.</p>
+          </div>
+        </div>
+      )
+    }}
+  </NxtWatchContext.Consumer>
+)
 
 export default NotFound
